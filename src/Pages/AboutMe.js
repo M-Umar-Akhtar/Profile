074527f477e461678b1cf38128dc9fa725317f7a
Umar Akhtar <umarakhtarms@gmail.com>
2023-06-23 05:34:26 +0500

    Transition on Cards

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,12 +1,14 @@
 import NavBar from "../Components/NavBar";
+import React from 'react';
+import { useState } from 'react'
 import '../assets/css/about.css'
 
-function Information(){
-    return(
-        <div className="aboutMe">
+function Information() {
+    return (
+        <div>
             <div className="infoContainer">
-                <h2><i class="fas fa-user" style={{fontSize: '20px', color: 'var(--variable-color)'}}></i> PERSONAL INFORMATION</h2>
-                <p>I am a passionate web developer. I have experience with Frontend and Backend of a website. 
+                <h2><i class="fas fa-user" style={{ fontSize: '20px', color: 'var(--variable-color)' }}></i> PERSONAL INFORMATION</h2>
+                <p>I am a passionate web developer. I have experience with Frontend and Backend of a website.
                     I am team player and understand the dynamics and working of a functional team. </p>
             </div>
             <div className="detailedInfoContainer">
@@ -34,26 +36,197 @@ function Information(){
     );
 }
 
-function Achivements(){
-    const education = () => {
-        return(
-            <div className="achivements">
+/*
+*
+*   Sample data for education, certificates and skills   
+* 
+*/
+const sampleData = {
+    EDUCATION: [
+        {
+            key: "1",
+            title: "BACHELOR'S OF SCIENCE IN SOFTWARE ENGINEERING - AIR UNIVERSITY ISLAMABAD",
+            date: "2019 - 2023",
+        },
+        {
+            key: "2",
+            title: "FSC PRE-ENGINEERING - ARMY PUBLIC SCHOOL AND COLLEGE",
+            date: "2017 - 2019",
+        },
+        {
+            key: "3",
+            title: "MATRIC - ARMY PUBLIC SCHOOL AND COLLEGE",
+            date: "2015 - 2017",
+        },
+    ],
+    CERTIFICATES: [
+        {
+            key: "3",
+            title: "HASH CODE 2022 - GOOGLE",
+            date: "2/2022 - 3/2022",
+        },
+        {
+            key: "4",
+            title: "MERN STACK ADVANCED - PSEB IT INDUSTRY ACADEMIA BRIDGE PROGRAM",
+            date: "7/2022 - 9/2022",
+        },
+    ]
+}
+function Cards() {
+
+    return (
+        <></>
+    );
+}
+
+function Achivements() {
+    // state to handle component render i:e education,certificate or skills
+    const styleButton = ["activeButton", "", ""];
+    const cardArray = ["first", "second", "third"];
+    const [cards, setCards] = useState(cardArray);
+    const [indexes, setIndex] = useState([0, 1, 2]);
+    const [currentCard, setCurrentCard] = useState(0);
+    const [currentButton, setCurrentButton] = useState(styleButton);
+    const [componentArray, setComponentArray] = useState(createArray());
+
+    /*
+*
+*   Education and certificates component    
+* 
+*/
+    function education_certificates(props) {
+        const { card, index, id, cardIndex } = props;
+        const icons = ["fas fa-graduation-cap", "fas fa-certificate"];
+        return (
+            <div id={id} className={cardIndex}>
                 <div className="titleContainer">
-                    <h2 className="title"><i class="fas fa-graduation-cap"></i> EDUCATION</h2>
+                    <h2 className="title"><i class={icons[index]}></i> {card}</h2>
                 </div>
-                <div className="">
+                <div className="certificate-education">
+                    <ul className="list">
+                        {sampleData[card].map(data => (
+                            <li className="item" key={data.key}>
+                                <h2>{data.title}</h2>
+                                <p><i className="far fa-calendar-alt" style={{ color: 'grey' }}></i> {data.date}</p>
+                                <hr></hr>
+                            </li>
+                        ))}
+                    </ul>
 
                 </div>
             </div>
         );
     }
+    /*
+    *
+    *   Skills component   
+    * 
+    */
+    function skills(cardIndex) {
+        return (
+            <div id="skl" className={cardIndex}>
+                <div className="titleContainer">
+                    <h2 className="title"><i class="fas fa-star"></i> SKILLS</h2>
+                </div>
+            </div>
+        );
+    }
+
+    /*
+    *
+    *   Main wrapper component  
+    * 
+    */
+
+    function onClick(index, value, id) {
+        console.log("ID == " + id)
+        setCurrentButton((prevArray) => {
+            const newArray = [...prevArray];
+            for (let i = 0; i < 3; i++) {
+                if (index === i)
+                    newArray[i] = value;
+                else
+                    newArray[i] = "";
+            }
+
+            return newArray;
+        });
+        setTimeout(() => {
+            setIndex(prevArray => {
+                const newArray = [...prevArray];
+                const currentIndex = newArray.indexOf(currentCard);
+                const newIndex = newArray.indexOf(id);
+                const temp = newArray[newIndex]
+                newArray[newIndex] = newArray[currentIndex];
+                newArray[currentIndex] = temp;
+                return newArray
+            });
+            setCurrentCard(id);
+        }, 300);
+    }
+    var components = [];
+    components.push(education_certificates({ card: "EDUCATION", index: 0, id: "edu", cardIndex: cards[0] }));
+    components.push(education_certificates({ card: "CERTIFICATES", index: 1, id: "cer", cardIndex: cards[1] }));
+    components.push(skills(cards[2]));
+
+    const createArray = () => {
+        let comps = [];
+        for (let i = 0; i < 3; i++) {
+            switch (indexes[i]) {
+                case 0:
+                    comps.push(education_certificates({ card: "EDUCATION", index: 0, id: "edu", cardIndex: cards[i] + " card" }));
+                    break;
+                case 1:
+                    comps.push(education_certificates({ card: "CERTIFICATES", index: 1, id: "cer", cardIndex: cards[i] + " card" }));
+                    break;
+                case 2:
+                    comps.push(skills(cards[i] + " card"));
+            }
+        }
+        return comps;
+    }
+
+    return (
+        <div className="achivements">
+            <div className="navigationContainer">
+                <div className="navigation">
+                    <button className={"navigationButton " + currentButton[0]} onClick={() => { onClick(0, "activeButton", 0) }}><i class="fas fa-graduation-cap"></i> EDUCATION</button>
+                    <button className={"navigationButton " + currentButton[1]} onClick={() => { onClick(1, "activeButton", 1) }}><i class="fas fa-certificate"></i> CERTIFICATES</button>
+                    <button className={"navigationButton " + currentButton[2]} onClick={() => { onClick(2, "activeButton", 2) }}><i class="fas fa-star"></i> SKILLS</button>
+                </div>
+            </div>
+            <div className="cardsContainer">
+                {createArray().map((element, index) => {
+                    return (
+                        <React.Fragment key={index}>
+                            {element}
+                        </React.Fragment>
+                    );
+                })}
+            </div>
+
+        </div>
+    );
+
 }
 
-export default function AboutMe(props){
-    return(
+export default function AboutMe(props) {
+    return (
         <>
-            <NavBar index={props.index}/>
-            <Information/>
+            <NavBar index={props.index} />
+            <div className="aboutMe">
+                <div className="headingContainer">
+                    <h1>ABOUT <span style={{ color: "var(--variable-color)" }}>ME</span></h1>
+                    <div className="breakerContainer">
+                        <hr className="hr"></hr>
+                        <i class="far fa-id-card" style={{ fontSize: '25px', color: 'var(--variable-color)' }}></i>
+                        <hr className="hr"></hr>
+                    </div>
+                </div>
+                <Information />
+                <hr className="hr" style={{ width: "100%", margin: "70px 0px" }}></hr>
+                <Achivements />
+            </div>
         </>
     );
 }
